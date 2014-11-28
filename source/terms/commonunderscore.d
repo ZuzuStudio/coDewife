91526ad6e774d363d8dc92686fe2b60889ec88c0
Harald Zealot <harald_zealot@tut.by>
2014-11-28 20:50:36 +0300

    all nedede terms for digit

---
+++ source/terms/commonunderscore.d
@@ -0,0 +1,31 @@
+﻿module terms.commonunderscore;
+
+public import terms.common;
+
+unittest
+{
+	assert(isOutputTerm!CommonUnderscore);
+}
+
+class CommonUnderscore: OutputTerm
+{
+public:
+	static bool printable;
+	
+	unittest
+	{
+		auto cu = new CommonUnderscore;
+		CommonUnderscore.printable = true;
+		assert("_" == cu.outputCharSequence);
+		CommonUnderscore.printable = false;
+		assert("" == cu.outputCharSequence);
+	}
+	
+	string outputCharSequence()@property
+	{
+		return printable ? "_" : "";
+	}
+}
+
+
+
