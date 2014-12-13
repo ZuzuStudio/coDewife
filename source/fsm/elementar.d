22d70348d660dc6b214c4dbbd12c7ea5157077a2
Harald Zealot <harald_zealot@tut.by>
2014-12-13 16:13:45 +0300

    some fixes and tests

--- source/fsm/elementar.d
+++ source/fsm/elementar.d
@@ -4,6 +4,24 @@ import fsm.common;
 import terms.invariantsequence;
 import std.conv;
 
+unittest
+{
+	Engine engine = new SingleIdentity("a");
+	size_t position = 0;
+	OutputTerm[] output;
+	assert(!engine.parse("A", position, output));
+	assert(!position);
+	assert(!output);
+	assert(!engine.parse("Ы", position, output));
+	assert(!position);
+	assert(!output);
+	assert(engine.parse("a", position, output));
+	assert(position == 1);
+	assert(output);
+	assert(output[0].outputCharSequence == "a");
+}
+
+
 final class SingleIdentity: Engine
 {
 public:
@@ -12,23 +30,38 @@ public:
 		key = symbol;
 	}
 
-	override bool parse(string text, ref size_t position, ref OutputTerm[] output)
-	{
-		if(position>=text.length)
-			return false;
-		size_t index = position;
-		auto symbol = to!string(decode(text, index));
-		auto result = symbol == key;
-		if(result)
-		{
-			position = index;
-			output ~= InvariantSequence(symbol);
-		}
-		return result;
-	}
+	mixin MixSimpleParse!(functor);
 
 private:
 	string key;
+	bool functor(string symbol){return symbol == this.key;} // чаму не працуе лямбда???
+}
+
+unittest
+{
+	Engine engine = new RangeIdentity("1","9");
+	size_t position = 0;
+	OutputTerm[] output;
+	assert(!engine.parse("0", position, output));
+	assert(!position);
+	assert(!output);
+	assert(!engine.parse("Ы", position, output));
+	assert(!position);
+	assert(!output);
+	assert(engine.parse("3", position, output));
+	assert(position == 1);
+	assert(output);
+	assert(output[0].outputCharSequence == "3");
+	position = 0;
+	assert(engine.parse("9", position, output));
+	assert(position == 1);
+	assert(output);
+	assert(output[1].outputCharSequence == "9");
+	position = 0;
+	assert(engine.parse("1", position, output));
+	assert(position == 1);
+	assert(output);
+	assert(output[2].outputCharSequence == "1");
 }
 
 final class RangeIdentity: Engine
@@ -41,10 +74,11 @@ public:
 		this.up = up;
 	}
 	
-	mixin MixSimpleParse!((string symbol) => down<=symbol && symbol <= up);
+	mixin MixSimpleParse!(functor);
 
 private:
 	string down, up;
+	bool functor(string symbol){return this.down <= symbol && symbol <= this.up;}
 }
 
 private mixin template MixSimpleParse(alias predicate)
@@ -59,13 +93,15 @@ private mixin template MixSimpleParse(alias predicate)
 		if(result)
 		{
 			position = index;
-			output ~= InvariantSequence(symbol);
+			output ~= new InvariantSequence(symbol);
 		}
 		return result;
 	}
 }
 
-final class AllIdentity: Engine
+
+// TODO implement for any passing symbol
+/*final class AllIdentity: Engine
 {
 
-}
+}*/
