c7960dc00011efced4e5691caac5addcc47031ce
Harald Zealot <harald_zealot@tut.by>
2014-12-28 20:11:56 +0300

    dummy

--- source/fsm/sequence.d
+++ source/fsm/sequence.d
@@ -1,1 +1,36 @@
- 
+module fsm.sequence;
+
+import fsm.common;
+
+unittest
+{
+	import std.traits;
+	import fsm.elementar;
+	static assert(__traits(compiles, {auto engine = new Sequence(new SingleIdentity("a"), new RangeIdentity("0","9"));}));
+	static assert(__traits(compiles, {auto engine = new Sequence("abc");}));
+	//auto engine = new Sequence(new SingleIdentity("a"), new RangeIdentity("0","9"));
+	auto engine = new Sequence("abc");
+	size_t position;
+	OutputTerm[] output;
+	assert(engine.parse("abc",position,output));
+}
+
+final class Sequence: Engine
+{
+public:
+	this(string keyString)
+	{
+		//TODO
+	}
+
+	this(Engine[] list...)
+	{
+		//TODO
+	}
+
+	bool parse(string text, ref size_t position, ref OutputTerm[] output)
+	{
+		//TODO
+		return false;
+	}
+}
