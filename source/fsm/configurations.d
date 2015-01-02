d9650c89859506eece8bd91ec68ffc4edeb4fcc1
Andrei Pazniak <krucios@mail.ru>
2015-01-02 21:50:23 +0000

    [~] Improved parallel fsm

--- source/fsm/configurations.d
+++ source/fsm/configurations.d
@@ -76,6 +76,57 @@ unittest
 	assert(output is null);
 }
 
+unittest
+{
+	import std.traits;
+
+	static assert(__traits(compiles, {auto engine = makeParallel(new SingleIdentity("a"), new RangeIdentity("0","9"));}));
+	
+	auto engine = makeParallel(new SingleIdentity("a"), new RangeIdentity("0","9"));
+	size_t position;
+	OutputTerm[] output;
+
+	position = 0;
+	output = null;
+	assert(engine.parse("a3",position,output));
+	assert(position == 1);
+	assert(output.charSequence == "a");
+	position = 0;
+	output = null;
+	assert(engine.parse("9a",position,output));
+	assert(position == 1);
+	assert(output.charSequence == "9");
+	position = 0;
+	output = null;
+	assert(!engine.parse("bac",position,output));
+	assert(position == 0);
+	assert(output is null);
+	position = 0;
+	output = null;
+	assert(engine.parse("3712",position,output));
+	assert(position == 1);
+	assert(output.charSequence == "3");
+	position = 0;
+	output = null;
+
+	engine = makeParallel(makeSequence("zuzu"), makeSequence(new RangeIdentity("0", "5"), makeSequence("_item")));
+	position = 0;
+	output = null;
+	assert(engine.parse("zuzu27",position,output));
+	assert(position == 4);
+	assert(output.charSequence == "zuzu");
+	position = 0;
+	output = null;
+	assert(engine.parse("2_items",position,output));
+	assert(position == 6);
+	assert(output.charSequence == "2_item");
+	position = 0;
+	output = null;
+	assert(!engine.parse("uzuzu",position,output));
+	assert(position == 0);
+	assert(output.charSequence == null);
+}
+
 final class Configurator: Engine
 {
 public:
@@ -122,5 +173,32 @@ auto makeSequence(Engine[] list)
 		current = newState;
 	}
 	current.addEdge(new Edge(new AllIdentity(), quasi), goodCrash);
+	return result;
+}
+
+Configurator makeParallel(Engine[] list...)
+{
+	return makeParallel(list);
+}
+
+auto makeParallel(Engine[] list)
+{
+	enum terminal = true;
+	enum nonterminal = false;
+	enum good = true;
+	enum bad = false;
+	enum quasi = true;
+	auto result = new Configurator();
+	result.start = new State(nonterminal, good);
+	auto goodCrash = new State(terminal, good);
+	auto badCrash = new State(terminal, bad);
+	State current = result.start;
+	auto goodState = new State(nonterminal, good);
+	goodState.addEdge(new Edge(new AllIdentity(), quasi), goodCrash);
+	foreach(engine; list)
+	{
+		current.addEdge(new Edge(engine), goodState);
+	}
+	current.addEdge(new Edge(new AllIdentity(), quasi), badCrash);
 	return result;
 }
