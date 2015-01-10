b3121e0fb7e70bd6364c84c26b4c9ab5155b6489
Harald Zealot <harald_zealot@tut.by>
2015-01-10 20:38:09 +0300

    [~] prepare to benchmark

--- skapp/intliteral/sample/intliteral.d
+++ skapp/intliteral/sample/intliteral.d
@@ -1,52 +1,7 @@
-module main;
+module sample.intliteral;
 
-import  std.stdio;
-
-import	terms.underscore;
-import	fsm.configurations;
-
-void main()
-{
-	Engine[] steam_engine;
-	steam_engine ~= makeDigitalLiteral();
-	steam_engine ~= makeAllIdentity();
-
-	auto line = readln();
-	line = line[0..$-1];
-	OutputTerm[] terms;
-	size_t position=0;
-	while(position < line.length)
-	{
-		if(steam_engine[0].parse(line, position, terms))
-		  continue;
-		steam_engine[1].parse(line, position, terms);
-	}
-
-	writeln("============= No Underscore ================");
-	LogicalUnderscore.printable = false;
-	UserUnderscore.printable = false;
-	CommonUnderscore.printable = false;
-	terms.output();
-	writeln("\n============================================");
-	writeln("============= User Underscore ==============");
-	LogicalUnderscore.printable = false;
-	UserUnderscore.printable = true;
-	CommonUnderscore.printable = true;
-	terms.output();
-	writeln("\n============================================");
-	writeln("============= Logical Underscore ===========");
-	LogicalUnderscore.printable = true;
-	UserUnderscore.printable = false;
-	CommonUnderscore.printable = true;
-	terms.output();
-	writeln("\n============================================");
-	writeln("============= Common Underscore ============");
-	LogicalUnderscore.printable = false;
-	UserUnderscore.printable = false;
-	CommonUnderscore.printable = true;
-	terms.output();
-	writeln("\n============================================");
-}
+import terms.underscore;
+import fsm.configurations;
 
 Engine makeDigitalLiteral()
 {
@@ -82,23 +37,4 @@ Engine makeDigitalLiteral()
                                                       makeQuantifier!backward(table["UU*D"], 0, 2));
   table["IntLiteral"] = makeHitherAndThither(table["ForwardIntLiteral"], table["BackwardIntLiteral"]);
   return table["IntLiteral"];
-}
-
-void output(OutputTerm[] terms)
-{
-	foreach(term; terms)
-	{
-		write(term.charSequence);
-	}
-	writeln("\n");
-	foreach(term; terms)
-	{
-		writef("%2s ", term.charSequence);
-	}
-	writeln();
-	foreach(term; terms)
-	{
-		write(term.id,":");
-	}
-	writeln();
 }
