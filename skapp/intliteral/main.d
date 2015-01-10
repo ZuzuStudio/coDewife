8e151197e82e2f955a3ddec15809146731d7af16
Harald Zealot <harald_zealot@tut.by>
2015-01-10 19:23:11 +0300

    [+] decimal literal

--- skapp/intliteral/main.d
+++ skapp/intliteral/main.d
@@ -46,7 +46,6 @@ void main()
 	CommonUnderscore.printable = true;
 	terms.output();
 	writeln("\n============================================");
-	readln();
 }
 
 Engine makeDigitalLiteral()
@@ -55,8 +54,34 @@ Engine makeDigitalLiteral()
   Engine[string] table;
   table["ForwardDigit"] = makeGeneral((string s) => "0" <= s && s <= "9",
                                       (string s) => cast(OutputTerm[])[]);
-  return makeGeneral((string s) => "0" <= s && s <= "9",
-                     (string s) => cast(OutputTerm[])[] ~ new InvariantSequence("*" ~ s));
+  table["ForwardUnderscore"] = makeGeneral((string s) => s == "_",
+                                           (string s) => cast(OutputTerm[])[]);
+  table["BackwardDigit"] = makeRangeIdentity!backward("0", "9");
+  table["BackwardLUDigit"] = makeGeneral!backward((string s) => "0" <= s && s <= "9",
+                                                  (string s) => cast(OutputTerm[])[] ~ new InvariantSequence(s) ~ new LogicalUnderscore);
+  table["XU"] = makeGeneral!backward((string s) => s == "_",
+                                     (string s) => cast(OutputTerm[])[] ~ new LastUnderscore);
+  table["CU"] = makeGeneral!backward((string s) => s == "_",
+                                     (string s) => cast(OutputTerm[])[] ~ new CommonUnderscore);
+  table["UU"] = makeGeneral!backward((string s) => s == "_",
+                                     (string s) => cast(OutputTerm[])[] ~ new UserUnderscore);
+  table["UU*D"] = makeSequence!backward(makeKleene!(star, backward)(table["UU"]),
+                                        table["BackwardDigit"]);
+  table["Period"] = makeSequence!backward(makeQuantifier!backward(table["UU*D"], 2),
+                                          makeParallel!backward(makeSequence!backward(table["CU"],
+                                                                                      makeKleene!(star, backward)(table["UU"]),
+                                                                                      table["BackwardDigit"]),
+                                                                table["BackwardLUDigit"]));
+                                                                              
+  table["ForwardIntLiteral"] = makeSequence(table["ForwardDigit"],
+                                            makeKleene!star(makeParallel(table["ForwardDigit"],
+                                                                         table["ForwardUnderscore"])));
+  table["BackwardIntLiteral"] = makeSequence!backward(makeKleene!(star, backward)(table["XU"]),
+                                                      table["BackwardDigit"],
+                                                      makeKleene!(star, backward)(table["Period"]),
+                                                      makeQuantifier!backward(table["UU*D"], 0, 2));
+  table["IntLiteral"] = makeHitherAndThither(table["ForwardIntLiteral"], table["BackwardIntLiteral"]);
+  return table["IntLiteral"];
 }
 
 void output(OutputTerm[] terms)
