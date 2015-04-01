818539adfe37d0f8b8926a541e1406f3d79056a5
Harald Zealot <harald_zealot@tut.by>
2015-04-01 20:26:11 +0300

    [+] dfs check

--- utils/grammarChecker/grammarchecker.d
+++ utils/grammarChecker/grammarchecker.d
@@ -6,8 +6,11 @@ import std.range;
 import std.traits;
 import std.algorithm;
 import std.array;
+import std.typecons;
 
 import jsonformatchecker;
+
+alias MarkableAutomat = Tuple!(string[], bool);
 
 immutable(dchar[]) jsonSymbol = ['[', ']', '{', '}', '\"', '\\', ':', ','];
 immutable(string[]) elementar = ["general", "singleIdentity", "rangeIdentity", "allIdentity"];
@@ -24,6 +27,9 @@ int main()
 	}
 
 	auto jsonTree = rawText.parseJSON;
+
+
+	MarkableAutomat[string] verteces;
 	foreach(automat; jsonTree.array)
 	{
 		check(("name" in automat), automat, "No name defined");
@@ -35,15 +41,35 @@ int main()
 		auto isForwardDirection = "direction" !in automat || automat["direction"].str == "forward"; 
 		writeln(automat["name"].str, "  (", automat["type"].str, isForwardDirection? " ==> ": " <== ", ")");
 
+		verteces[automat["name"].str] = tuple(null, false);
+
 		if(canFind(configurations, automat["type"].str))
 		{
 			check(("automata" in automat), automat, "No automata for configuration defined");
 			check(automat["automata"].type == JSON_TYPE.ARRAY, automat, "Field automata isn't array");
+			string[] automataString;
 			foreach(subautomat; automat["automata"].array)
+			{
 				writeln("\t", subautomat.str);
+				automataString ~= subautomat.str;
+			}
+			verteces[automat["name"].str][0] = automataString;
 		}
 
 	}
+
+	foreach(pair; verteces.byKeyValue)
+		writeln(pair.key, ": ", pair.value[0], " ", pair.value[1]);
+
+	writeln();
+
+	dfs(verteces, jsonTree.array[0]["name"].str);
+
+	writeln();
+
+	foreach(pair; verteces.byKeyValue)
+		writeln(pair.key, ": ", pair.value[0], " ", pair.value[1]);
+
 	return 0;
 }
 
@@ -55,6 +81,21 @@ void check(T)(T predicate, lazy JSONValue jsonValue, lazy string message)
 		stderr.writeln(jsonValue.toPrettyString());
 		stderr.writeln(message);
 		core.stdc.stdlib.exit(1);
+	}
+}
+
+void dfs(ref MarkableAutomat[string] vertices, string current)
+{
+	if(!vertices[current][1])
+	{
+		vertices[current][1] = true;
+		foreach(next; vertices[current][0])
+		{
+			if(next in vertices)
+				dfs(vertices, next);
+			else
+				stderr.writeln(current, " reference to node ", next, " that doesn't exist");
+		}
 	}
 }
 
